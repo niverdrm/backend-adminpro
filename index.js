7ea036b36d4e4b055ca63bf0874ea72fc99e895a
NiverDaniel <niverdanielrm@ufps.edu.co>
2022-03-07 15:24:25 -0500

    control de SPA

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+const path = require("path");
+
 const express = require("express");
 const cors = require("cors");
 const { dbConexion } = require("./databese/config");
@@ -26,6 +28,11 @@ app.use("/api/medicos", require("./routes/medicos"));
 app.use("/api/todo", require("./routes/busquedas"));
 app.use("/api/login", require("./routes/auth"));
 app.use("/api/upload", require("./routes/uploads"));
+
+// lo ultimo
+app.get("*", (req, resp) => {
+  resp.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log("port", process.env.PORT);
